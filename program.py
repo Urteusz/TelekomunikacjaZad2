9c4d39d112fe1370140254914fce1565f64d316e
Urteusz <mateusz.urbaniak.23@gmail.com>
2025-04-22 20:01:12 +0200

    Remove redundant debug print statements in program.py to clean up output during file transfer

--- program.py
+++ program.py
@@ -62,28 +62,9 @@ def receive_file(port, filename, use_crc=True):
             ser.close()
             return
 
-
-    # init_char = C if use_crc else NAK
-    #
-    #
-    # ser.write(bytes([init_char]))
-    # print(f"[Odbiornik] Ponowiono inicjalizację: {hex(init_char)}")
-    # next_init_time = time.time()
-    # while True:
-    #     now = time.time()
-    #     # co sekundę ponawiaj inicjalizację
-    #     if now >= next_init_time:
-    #         ser.write(bytes([init_char]))
-    #         next_init_time = now + 1
-    #         print(f"[Odbiornik] Ponowiono inicjalizację: {hex(init_char)}")
-    #     header = ser.read(1)
-    #     if not header:
-    #         break
-
     expected_block = 1
     with open(filename, 'wb') as f:
         while True:
-            print("[Odbiornik] Czekam na nagłówek lub EOT...")
             header = ser.read(1)
             if not header:
                 print("[Odbiornik] Timeout - brak nagłówka")
@@ -91,10 +72,8 @@ def receive_file(port, filename, use_crc=True):
 
             code = header[0]
             if code == SOH:
-                print(f"[Odbiornik] Odebrano SOH, oczekiwany blok: {expected_block}")
                 block_num = ser.read(1)[0]
                 block_num_comp = ser.read(1)[0]
-                print(f"[Odbiornik] Numer bloku: {block_num}, komplement: {block_num_comp}")
 
                 if (block_num + block_num_comp) & 0xFF != 0xFF:
                     print("[Odbiornik] Błąd numeru bloku, wysyłam NAK")
@@ -110,11 +89,9 @@ def receive_file(port, filename, use_crc=True):
                 if use_crc:
                     received_crc = int.from_bytes(ser.read(2), 'big')
                     calc_crc = calculate_crc(data)
-                    print(f"[Odbiornik] Otrzymane CRC: {received_crc}, obliczone CRC: {calc_crc}")
                     if received_crc == calc_crc:
                         f.write(data)
                         ser.write(bytes([ACK]))
-                        print(f"[Odbiornik] Blok {block_num} OK, wysłano ACK")
                         expected_block += 1
                     else:
                         print("[Odbiornik] CRC niezgodne, wysłano NAK")
@@ -122,11 +99,9 @@ def receive_file(port, filename, use_crc=True):
                 else:
                     received_sum = ser.read(1)[0]
                     calc_sum = calculate_checksum(data)
-                    print(f"[Odbiornik] Otrzymane checksum: {received_sum}, obliczone checksum: {calc_sum}")
                     if received_sum == calc_sum:
                         f.write(data)
                         ser.write(bytes([ACK]))
-                        print(f"[Odbiornik] Blok {block_num} OK, wysłano ACK")
                         expected_block += 1
                     else:
                         print("[Odbiornik] Checksum niezgodne, wysłano NAK")
@@ -167,7 +142,6 @@ def send_file(port, filename, use_crc=True):
                 print("[Nadawca] Brak danych, przechodzę do EOT")
                 break
             data = data.ljust(BLOCK_SIZE, b'\x1A')
-            print(f"[Nadawca] Przygotowuję blok {block_number}")
 
             # Nagłówek i dane
             packet = bytes([SOH, block_number, 255 - block_number]) + data
@@ -180,7 +154,6 @@ def send_file(port, filename, use_crc=True):
 
             # Wysyłka i oczekiwanie na ACK/NAK
             while True:
-                print(f"[Nadawca] Wysyłam blok {block_number}")
                 ser.write(packet)
                 resp = ser.read(1)
                 if not resp:
